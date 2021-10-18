ebae479f2eab46f7d4fa4c045c89bdf39c265209
Tom Schwarz <hi@tomschwarz.info>
2021-10-18 11:42:12 +0200

    Format code, add .gitignore

--- main.go
+++ main.go
@@ -1,57 +1,57 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net"
-	"net/http"
-	"os"
+    "flag"
+    "fmt"
+    "os"
 )
 
+// gocrt version
 var gocrtVersion = "dev"
 
+// init, get called automatic before main()
 func init() {
-	flag.Usage = func() {
-		h := "A crt.sh command line client written in golang.\n\n"
+    flag.Usage = func() {
+        h := "A crt.sh command line client written in golang.\n\n"
 
-		h += "Usage:\n"
-		h += "  gocrt [OPTIONS] [FILE|URL|-]\n\n"
+        h += "Usage:\n"
+        h += "  gocrt [OPTIONS] [FILE|URL|-]\n\n"
 
-		h += "Options:\n"
-		h += "  -o, --output     Output directory for all found subdomains of given domains\n"
-		h += "      --version    Print version information\n"
-		h += "\n"
+        h += "Options:\n"
+        h += "  -h, --help       Print usage informations\n"
+        h += "  -o, --output     Output directory for all found subdomains of given domains\n"
+        h += "      --version    Print version information\n"
+        h += "\n"
 
-		h += "Examples:\n"
-		h += "  cat domains.txt | gocrt -o domains-crt\n"
-		h += "  gocrt -o domains-crt example.com \n"
-		h += "  gocrt < domains.txt\n"
-		h += "  gocrt example.com\n"
+        h += "Examples:\n"
+        h += "  cat domains.txt | gocrt -o domains-crt\n"
+        h += "  gocrt -o domains-crt example.com \n"
+        h += "  gocrt < domains.txt\n"
+        h += "  gocrt example.com\n"
 
-		fmt.Fprintf(os.Stderr, h)
-	}
+        fmt.Fprintf(os.Stderr, h)
+    }
 }
 
-func main() {	
-	// "version" command line argument
-	var version bool
-	flag.BoolVar(&version, "version" false, "")
-	
-	// "output" command line argument
-	var output string
-	flag.StringVar(&output, "output", "gocrt", "")
-	flag.StringVar(&output, "o", "gocrt", "")
+// main, magic happens here
+func main() {
+    // "version" command line argument
+    var version bool
+    flag.BoolVar(&version, "version", false, "")
 
-	flag.Parse()
-	
-	// Print version
-	if version {
-		fmt.Printf("gocrt version %s\n", gocrtVersion)
-		os.Exit(0)
-	}
-	
-	// TESTING
-	fmt.Printf("-o or --output argument: $s\n", output)
+    // "output" command line argument
+    var output string
+    flag.StringVar(&output, "output", "gocrt", "")
+    flag.StringVar(&output, "o", "gocrt", "")
+
+    flag.Parse()
+
+    // Print version
+    if version {
+        fmt.Printf("gocrt version: %s\n", gocrtVersion)
+        os.Exit(0)
+    }
+
+    // TESTING
+    fmt.Printf("-o or --output argument: %s\n", output)
 }
