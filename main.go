c370fdbb1ad6fabacb8ca64e44e3634267c094ec
Tom Schwarz <hi@tomschwarz.info>
2021-10-18 11:27:38 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,18 +10,48 @@ import (
 	"os"
 )
 
-func help() {
+var gocrtVersion = "dev"
 
+func init() {
+	flag.Usage = func() {
+		h := "A crt.sh command line client written in golang.\n\n"
+
+		h += "Usage:\n"
+		h += "  gocrt [OPTIONS] [FILE|URL|-]\n\n"
+
+		h += "Options:\n"
+		h += "  -o, --output     Output directory for all found subdomains of given domains\n"
+		h += "      --version    Print version information\n"
+		h += "\n"
+
+		h += "Examples:\n"
+		h += "  cat domains.txt | gocrt -o domains-crt\n"
+		h += "  gocrt -o domains-crt example.com \n"
+		h += "  gocrt < domains.txt\n"
+		h += "  gocrt example.com\n"
+
+		fmt.Fprintf(os.Stderr, h)
+	}
 }
 
-func main() {
-	var help bool
-	flag.BoolVar(&help, "h", false, "Prints the help page") 
+func main() {	
+	// "version" command line argument
+	var version bool
+	flag.BoolVar(&version, "version" false, "")
+	
+	// "output" command line argument
+	var output string
+	flag.StringVar(&output, "output", "gocrt", "")
+	flag.StringVar(&output, "o", "gocrt", "")
 
 	flag.Parse()
-
-	if help {
-	    help()
-	    exit
+	
+	// Print version
+	if version {
+		fmt.Printf("gocrt version %s\n", gocrtVersion)
+		os.Exit(0)
 	}
+	
+	// TESTING
+	fmt.Printf("-o or --output argument: $s\n", output)
 }
